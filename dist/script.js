2f15308463d2743a769d9d2621e0f5e54239bb4c
OC-kyle-solomons <kyle.solomons@student.onslow.school.nz>
2025-06-29 20:07:41 +1200

    Fix: Correct code

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,35 @@
 "use strict";
+const spawnButton = document.getElementById('spawn');
+
+spawnButton.addEventListener('click', () => {
+    const img = document.createElement('img');
+    img.src = "assets/fishclosed.png";
+    img.style.position = 'absolute';
+    img.style.left = Math.random() * (window.innerWidth - 100) + 'px';
+    img.style.top = Math.random() * (window.innerHeight - 100) + 'px';
+    img.isalive = true;
+    img.addEventListener('click', () => {
+        img.isalive = false;
+        img.src = "assets/fishdead.png";
+        setTimeout(() => {
+            img.remove();
+        }, 1000);
+    });
+
+    img.addEventListener('mouseover', () => {
+    if (img.isalive) {
+        img.src = "assets/fishhover.png";
+    }
+    
+    });
+
+    img.addEventListener('mouseout', () => {
+    if (img.isalive) {
+        img.src = "assets/fishclosed.png";
+    }
+    });
+
+
+
+    document.body.appendChild(img);
+});
